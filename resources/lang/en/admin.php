70cf5fe8230f75745332e4ff5d9a3f2f2a0d9d22
Franck1stein <franck.adokou@gmail.com>
2019-11-13 07:00:19 +0000

    initial commit after the wrong branch merge

--- resources/lang/en/admin.php
+++ resources/lang/en/admin.php
@@ -1,5 +1,7 @@
 <?php
   return [
-    'login'                       => 'login',
-    'incorrect_information_login' => 'Email or Password Incorrect Please try again'
+                          'login' => 'login',
+    'incorrect_information_login' => 'Email or Password Incorrect Please try again',
+                'forgot_password' => 'Forgot password',
+            'the_link_reset_sent' => 'Reset Link is sent',
   ];
